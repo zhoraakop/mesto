e067d380d318df0ea01bf07519184f32846168a5
Georgy Akopov <akopovzh@yandex.ru>
2023-07-21 12:30:25 +0300

    fixed/review/all

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -8,17 +8,17 @@ export class Popup{
 
     open(){
         this._selector.classList.add('popup_opened');
-        document.addEventListener('keydown', (evt) => this._handleEscClose(evt));
+        document.addEventListener('keydown', this._handleEscClose);
     }
 
     close(){
         this._selector.classList.remove('popup_opened');
-        document.removeEventListener('keydown', (evt) => this._handleEscClose(evt));
+        document.removeEventListener('keydown', this._handleEscClose);
     }
 
     
 
-    _handleEscClose(evt){
+    _handleEscClose = (evt) => {
         if (evt.key === 'Escape') {
             this.close();
         };
